f596297dd319c6c94a38bfaa244d679017f733ed
pivotal-preston-sheldon <psheldon@pivotal.io>
2018-03-02 13:47:37 -0500

    Add https connector configuration

--- src/main/java/com/example/bookpub/config/WebConfiguration.java
+++ src/main/java/com/example/bookpub/config/WebConfiguration.java
@@ -2,13 +2,19 @@ package com.example.bookpub.config;
 
 import com.example.bookpub.formatter.BookFormatter;
 import com.example.bookpub.repository.BookRepository;
+import org.apache.catalina.connector.Connector;
 import org.apache.catalina.filters.RemoteIpFilter;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.http.converter.ByteArrayHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageConverter;
@@ -18,10 +24,13 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
+import java.io.File;
 import java.time.Duration;
 import java.util.List;
 
 @Configuration
+@PropertySource("classpath:/tomcat.https.properties")
+@EnableConfigurationProperties(WebConfiguration.TomcatSslConnectorProperties.class)
 public class WebConfiguration implements WebMvcConfigurer {
 
     private Log logger = LogFactory.getLog(WebConfiguration.class);
@@ -42,6 +51,20 @@ public class WebConfiguration implements WebMvcConfigurer {
     @Bean
     public ByteArrayHttpMessageConverter byteArrayHttpMessageConverter() {
         return new ByteArrayHttpMessageConverter();
+    }
+
+    @Bean
+    public ServletWebServerFactory servletContainer
+            (TomcatSslConnectorProperties properties) {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createSslConnector(properties));
+        return tomcat;
+    }
+
+    private Connector createSslConnector(TomcatSslConnectorProperties properties) {
+        Connector connector = new Connector();
+        properties.configureConnector(connector);
+        return connector;
     }
 
     @Bean
@@ -82,5 +105,87 @@ public class WebConfiguration implements WebMvcConfigurer {
         registry.addResourceHandler("/internal/**").addResourceLocations("classpath:/");
 
     }
+
+    @ConfigurationProperties(prefix = "custom.tomcat.https")
+    public static class TomcatSslConnectorProperties {
+
+        private Integer port;
+        private Boolean ssl = true;
+        private Boolean secure = true;
+        private String scheme = "https";
+        private File keystore;
+        private String keystorePassword;
+
+
+        public void configureConnector(Connector connector) {
+            if (port != null) {
+                connector.setPort(port);
+            }
+            if (secure != null) {
+                connector.setSecure(secure);
+            }
+            if (scheme != null) {
+                connector.setScheme(scheme);
+            }
+            if (ssl != null) {
+                connector.setProperty("SSLEnabled", ssl.toString());
+            }
+            if (keystore != null && keystore.exists()) {
+                connector.setProperty("keystoreFile",keystore.getAbsolutePath());
+            }
+            if (keystorePassword != null) {
+                connector.setProperty("keystorePassword", keystorePassword);
+            }
+        }
+
+        public Integer getPort() {
+            return port;
+        }
+
+        public void setPort(Integer port) {
+            this.port = port;
+        }
+
+        public Boolean getSsl() {
+            return ssl;
+        }
+
+        public void setSsl(Boolean ssl) {
+            this.ssl = ssl;
+        }
+
+        public Boolean getSecure() {
+            return secure;
+        }
+
+        public void setSecure(Boolean secure) {
+            this.secure = secure;
+        }
+
+        public String getScheme() {
+            return scheme;
+        }
+
+        public void setScheme(String scheme) {
+            this.scheme = scheme;
+        }
+
+        public File getKeystore() {
+            return keystore;
+        }
+
+        public void setKeystore(File keystore) {
+            this.keystore = keystore;
+        }
+
+        public String getKeystorePassword() {
+            return keystorePassword;
+        }
+
+        public void setKeystorePassword(String keystorePassword) {
+            this.keystorePassword = keystorePassword;
+        }
+    }
+
 
 }
