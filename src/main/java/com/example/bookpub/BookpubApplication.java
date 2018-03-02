7d3ea3bbc1a9a703551ebc8d3e29303a271dca32
pivotal-preston-sheldon <psheldon@pivotal.io>
2018-03-02 15:37:16 -0500

    Enable Auto-configuration using an Annotation

--- src/main/java/com/example/bookpub/BookpubApplication.java
+++ src/main/java/com/example/bookpub/BookpubApplication.java
@@ -1,6 +1,7 @@
 package com.example.bookpub;
 
 import com.example.bookpub.config.StartupRunner;
+import com.example.bookpubstarter.dbcount.EnableDbCounting;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableDbCounting
 public class BookpubApplication {
 
 //	protected final Log logger = LogFactory.getLog(getClass());
--- src/main/java/com/example/bookpub/config/StartupRunner.java
+++ src/main/java/com/example/bookpub/config/StartupRunner.java
@@ -12,8 +12,11 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.util.Assert;
 
+@Order(Ordered.LOWEST_PRECEDENCE - 1)
 public class StartupRunner implements CommandLineRunner {
     protected final Log logger = LogFactory.getLog(getClass());
 
