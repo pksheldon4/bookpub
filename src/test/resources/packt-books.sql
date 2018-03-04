7ced537e4e4b6c0b8f51536635a89d862776609b
pivotal-preston-sheldon <psheldon@pivotal.io>
2018-03-03 22:10:44 -0500

    Update book insert to use dynamic ID value

--- src/test/resources/packt-books.sql
+++ src/test/resources/packt-books.sql
@@ -1,2 +1,2 @@
 INSERT INTO author (id, first_name, last_name) VALUES (5, 'Shrikrishna', 'Holla')
-INSERT INTO book (id, isbn, title, author_id, publisher_id) VALUES (5,'978-1-78398-478-7', 'Orchestrating Docker', 5, 1)
+INSERT INTO book (isbn, title, author_id, publisher_id) VALUES ('978-1-78398-478-7', 'Orchestrating Docker', 5, 1)
